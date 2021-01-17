51fd11bdf23406bd481bf329b80385fb9c536e43
Andreea-Luiza <74903612+Andreea-Luiza@users.noreply.github.com>
2021-01-17 20:25:24 +0200

    Update TABELA.h

--- TABELA.h
+++ TABELA.h
@@ -10,7 +10,7 @@ class TABELA
 	friend class Menu;
 public:
 
-	void CREATE_TABLE(TABELA*,string, string*, int, string*, int, int*);
+	void CREATE_TABLE(TABELA*, string, string*, int, string*, int, int*);
 	string* getNume_Coloana();
 	string* getTip_coloana();
 	int* getDimensiune();
@@ -18,26 +18,26 @@ public:
 	void setNume_Coloana(string*, int, string*, int, int*);
 	void setNume_Tabela(string);
 	TABELA();
-	~TABELA();
+	virtual ~TABELA();
 	TABELA(const TABELA&);
 	TABELA(string*, int, string*, int, int*);
 	TABELA& operator=(const TABELA&);
 	void DROP_TABLE(TABELA*);
-	void DISPLAY(TABELA*,string);
+	void DISPLAY(TABELA*, string);
+	virtual void RENAME(TABELA*, string);
 	friend ostream& operator<<(ostream&, TABELA);
 	friend istream& operator>>(istream&, TABELA&);
-	void serializare();
+	void serializare(string, ofstream&);
 	void deserializare();
-
+	void REDIMENSIONARE(TABELA*,string);
 	friend ofstream& operator<<(ofstream&, TABELA);
 	friend ifstream& operator>>(ifstream&, TABELA&);
 	string nume_tabela;
 private:
-	
+
 	int numar_coloane = 0;
 	string* nume_coloana = nullptr;
 	int numar_tip_coloana = 0;
 	string* tip_coloana = nullptr;
 	int* dimensiune = nullptr;
 };
-
